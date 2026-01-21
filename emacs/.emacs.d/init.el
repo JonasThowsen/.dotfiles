955b94f411fa9db053f543d53599ae2eac28e007
JonasThowsen <jonas.aa.thowsen@gmail.com>
2026-01-21 20:42:57 +0100

    More Emacs config stuff
    Add general for custom evil keybinds
    Add avy
    Add some theming config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,6 +2,21 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(setq make-backup-files nil)
+
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+;;; Theme and font
+(load-theme 'rosebones t)
+(set-frame-font "0xProto-14" t t)
+
+;;; General
+(require 'general)
+
+;;; Evil mode configuration
 (setq evil-want-C-u-scroll t)
 (setq evil-want-keybinding nil)
 (require 'evil)
@@ -10,3 +25,18 @@
   (evil-collection-init))
 
 (evil-set-undo-system 'undo-redo)
+
+;; General evil stuff
+(general-evil-setup t)
+
+(general-create-definer my-leader
+  :states '(normal visual motion)
+  :keymaps 'override
+  :prefix "SPC"
+  :global-prefix "C-SPC")
+
+(my-leader
+  "gg" 'magit)
+
+(my-leader
+  "a" 'avy-goto-char-timer)
