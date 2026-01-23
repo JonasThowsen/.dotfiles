0cb2b4dfd7b45467c02a87397fe3e1599348193a
JonasThowsen <jonas.aa.thowsen@gmail.com>
2026-01-23 15:56:37 +0100

    Add clipboard support between wayland and Emacs
    UTF-8 did not work when pasting text from Emacs before this.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,25 @@
+;;; Wayland clipboard support (for wl-clipboard)
+(setq wl-copy-process nil)
+
+(defun wl-copy (text)
+  "Copy TEXT to Wayland clipboard using wl-copy."
+  (setq wl-copy-process (make-process :name "wl-copy"
+                                       :buffer nil
+                                       :command '("wl-copy" "-f" "-n")
+                                       :connection-type 'pipe
+                                       :noquery t))
+  (process-send-string wl-copy-process text)
+  (process-send-eof wl-copy-process))
+
+(defun wl-paste ()
+  "Paste from Wayland clipboard using wl-paste."
+  (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil
+    (shell-command-to-string "wl-paste -n 2>/dev/null")))
+
+(setq interprogram-cut-function 'wl-copy)
+(setq interprogram-paste-function 'wl-paste)
+
 ;;; Helper functions
 (defun open-file-in-new-tab (file)
   "Open FILE in a new tab."
