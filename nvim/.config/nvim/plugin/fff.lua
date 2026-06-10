e350cc18826548aff6202c4acca4fac9d6f814e1
JonasThowsen <jonas.aa.thowsen@gmail.com>
2026-06-10 12:32:38 +0200

    Use local fff fork

--- nvim/.config/nvim/plugin/fff.lua
+++ nvim/.config/nvim/plugin/fff.lua
@@ -1,55 +1,54 @@
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    local data = ev.data
-    if not data or (data.kind ~= 'install' and data.kind ~= 'update') then
-      return
-    end
+vim.api.nvim_create_autocmd("PackChanged", {
+	callback = function(ev)
+		local data = ev.data
+		if not data or (data.kind ~= "install" and data.kind ~= "update") then
+			return
+		end
 
-    local spec = data.spec
-    local path = data.path
-    if spec.name ~= 'fff.nvim' or not path then
-      return
-    end
+		local spec = data.spec
+		local path = data.path
+		if spec.name ~= "fff.nvim" or not path then
+			return
+		end
 
-    local result = vim.system(
-      { 'nix', 'run', '.#release' },
-      { cwd = path }
-    ):wait()
+		local result = vim.system({ "nix", "run", ".#release" }, { cwd = path }):wait()
 
-    if result.code == 0 then
-      vim.notify('fff.nvim built with nix', vim.log.levels.INFO)
-      return
-    end
+		if result.code == 0 then
+			vim.notify("fff.nvim built with nix", vim.log.levels.INFO)
+			return
+		end
 
-    error('fff.nvim nix build failed:\n' .. (result.stderr or 'unknown error'))
-  end,
+		error("fff.nvim nix build failed:\n" .. (result.stderr or "unknown error"))
+	end,
 })
 
-vim.pack.add({ 'https://github.com/dmtrKovalenko/fff.nvim' })
+local fff_local_path = "/home/jonas/coding/fff.nvim"
+vim.opt.runtimepath:prepend(fff_local_path)
+
+vim.pack.add({
+	{
+		src = fff_local_path,
+		name = "fff.nvim",
+	},
+})
 
 vim.g.fff = {
-  lazy_sync = true,
-    prompt = 'λ ',
-    layout = {
-      height = 0.9,
-      width = 0.9,
-    }
+	lazy_sync = true,
+	prompt = "λ ",
+	layout = {
+		height = 0.9,
+		width = 0.9,
+	},
 }
 
-vim.keymap.set(
-  'n',
-  '<leader>f',
-  function() require('fff').find_files() end,
-  { desc = 'FFFind files' }
-)
+vim.keymap.set("n", "<leader>f", function()
+	require("fff").find_files()
+end, { desc = "FFFind files" })
 
-vim.keymap.set(
-  'n',
-  '<leader>s',
-  function() require('fff').live_grep({
-	  grep = {
-		  modes = {'fuzzy', 'regex', 'plain'}
-	  }
-  }) end,
-  { desc = 'FFFind files' }
-)
+vim.keymap.set("n", "<leader>s", function()
+	require("fff").live_grep({
+		grep = {
+			modes = { "fuzzy", "regex", "plain" },
+		},
+	})
+end, { desc = "FFFind files" })
