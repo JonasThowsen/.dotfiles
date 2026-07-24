6b39ca28c3fa7936cb0983951f553ec0a86fc397
JonasThowsen <jonas.aa.thowsen@gmail.com>
2026-07-24 23:41:22 +0200

    Enable truecolor in Neovim

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,1 +1,4 @@
-vim.cmd([[colorscheme catppuccin]])
+-- The builtin Catppuccin theme otherwise falls back to its 256-color
+-- palette in remote tmux and Mosh sessions.
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("catppuccin")
